7c4f4d727441ca9eec93e9b757a4a1534e4737ba
bsc-44-21 <bsc-44-21@unima.ac.mw>
2026-01-20 03:02:21 +0200

    Allow custom User-Agent

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ if (process.env.YOUTUBE_COOKIES) {
 const commonArgs = [
   '--no-check-certificates',
   '--user-agent',
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+  process.env.USER_AGENT || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
   '--ignore-errors',
   '--no-warnings',
   '--restrict-filenames',
